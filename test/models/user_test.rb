bcb19268c77ea90e2b01c76d9974d7fd7c483b5a
otamm <oivatom@gmail.com>
2015-01-16 03:24:37 -0200

    Added tests for user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
-class UserTest < ActiveSupport::TestCase
+class UserTest < ActiveSupport::TestCase #on terminal: $bundle exec rake test:models OR rake test:models
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user = User.new(name: "Mr Celophane", email: "mr@celophane.net")
+  end
+
+  test "user is valid" do
+    assert @user.valid? #assert: makes sure that its argument (@user.valid?) returns 'true'
+  end
+
+  test "user is not valid" do
+    @user.name = "     "
+    assert_not @user.valid? #assert_not: makes sure that its argument returns 'false'
+  end
+
+  test "user is not valid" do
+    @user.email = "hello,world"
+    assert_not @user.valid?
+  end
 end
