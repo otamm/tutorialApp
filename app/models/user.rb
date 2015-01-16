1ea2257c4ea296a7325b0617f0981858e8b7909a
otamm <oivatom@gmail.com>
2015-01-16 03:44:21 -0200

    updated tests for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessor :name, :email
+  # on a 'rails console --sandbox', if 'user.valid? == false' use 'user.errors.full_messages' to check for the specific error message.
+  validates(:name, presence: true) #equivalent to "user.save if user.name.size > 0" in logic, the 'validates()' method also add errors to the screen according to the argument passed ('name can't be blank') for (:name,presence: true)
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -12,13 +12,33 @@ class UserTest < ActiveSupport::TestCase #on terminal: $bundle exec rake test:mo
     assert @user.valid? #assert: makes sure that its argument (@user.valid?) returns 'true'
   end
 
-  test "user is not valid" do
-    @user.name = "     "
+  test "user without name is not valid" do
+    @user.name = ""
     assert_not @user.valid? #assert_not: makes sure that its argument returns 'false'
   end
 
-  test "user is not valid" do
+  test "user without email is not valid" do
+    @user.email = ""
+    assert_not @user.valid?
+  end
+
+  test "user without proper name is not valid" do
+    @user.name = "      "
+    assert_not @user.valid?
+  end
+
+  test "user without proper email is not valid" do
     @user.email = "hello,world"
+    assert_not @user.valid?
+  end
+
+  test "user with a very long name is not valid" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "user with a very long email is not valid" do
+    @user.email = "a" + "@" + "a" * 252 + ".com"
     assert_not @user.valid?
   end
 end
