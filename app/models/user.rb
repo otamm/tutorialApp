61f6fd049da5cb55b28f725ddcf05c9199553382
otamm <oivatom@gmail.com>
2015-01-16 04:21:43 -0200

    Added security for password protection in the db

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessor :name, :email
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i #constant for the RegEx that validates the e-mail.
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i #constant for the RegEx that validates the e-mail; more info on README.md
 
   # on a 'rails console --sandbox', if 'user.valid? == false' use 'user.errors.full_messages' to check for the specific error message.
   validates(:name, presence: true, length: {maximum: 50}) # comments on each argument are below.
@@ -10,4 +10,8 @@ class User < ActiveRecord::Base
   validates(:email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}) #see comments above for each argument for the 'validates()' method,the ones for 'format' & 'uniqueness' is below.
   #(format: {with: "..."}): checks wheter the input matches a pattern. '{without: "..."}' checks wheter the input does not match a certain pattern.
   #(uniqueness: true): obviously checks for uniqueness in the DB; a duplicat should not be valid, even if written with a different pattern of upper/downcase chars.
+
+  before_save({self.email = self.email.downcase}) #guarantees the email will be saved in a standardized downcase format.
+
+  has_secure_password #method used with bcrypt gem, which uses cryptographic hash functions before storing a password (http://en.wikipedia.org/wiki/Hash_function)
 end
---
+++ db/migrate/20150116061754_add_secure_passwords_to_users.rb
@@ -0,0 +1,5 @@
+class AddSecurePasswordsToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :password_digest, :string
+  end
+end
