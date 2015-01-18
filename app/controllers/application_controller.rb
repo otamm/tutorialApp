86d68f292fcd57ec50a814fbf9ecaedeba546f0a
otamm <oivatom@gmail.com>
2015-01-18 18:09:42 -0200

    Added log_in helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  include SessionsHelper # including all the helper methods for Sessions controller on the universal controller ApplicationController, as they'll be used through the entire app.
+   
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,7 @@
 module SessionsHelper
+
+  def log_in(user) #this method below (session[]) is default in Rails,it has nothing to do with the Sessions controller.
+    session[:user_id] = user.id # generates a session cookie that is destroyed as soon as the session closes (user closes the browser/logs out).
+  end                           # [:user_id] attribute is assigned to the id of the user passed as a parameter to log_int method, id is automatically encrypted and retrieved using the encrypt/unencrypt algorithm provided by sessions[user_id].
+
 end
