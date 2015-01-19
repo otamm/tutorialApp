7c650e9beef477cf5f5a3175918d2480fa5a056d
otamm <oivatom@gmail.com>
2015-01-19 03:50:04 -0200

    Added methods to generate secure cookie tokens

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :email, :password, :password_confirmation
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_token
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i #constant for the RegEx that validates the e-mail; more info on README.md
   has_secure_password #method used with bcrypt gem, which uses cryptographic hash functions before storing a password (http://en.wikipedia.org/wiki/Hash_function)
 
@@ -16,10 +16,23 @@ class User < ActiveRecord::Base
 
   before_save {self.email = self.email.downcase} #guarantees the email will be saved in a standardized downcase format. note that 'before_save' uses a block as input if it is needed to assign a variable.
 
-  def User.digest(string) # this methods creates an user fixture (a valid object to be assigned to a test database in order to run tests).
+  def User.digest(string) # this method creates an user fixture (a valid object to be assigned to a test database in order to run tests).
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost # the 'cost' is the computational cost to decypher the string. A low cost makes it easy, a very high cost, nearly impossible.
 
     BCrypt::Password.create(string, cost: cost) # the value returned will be the string passed as an argument to User.digest() encrypted at the lowest computational cost possible, as it will be only used for testing (the computational cost for production mode will be set to high)
+  end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64 #'base64' before the generated string(will have .size == 22) will be composed by a-zA-Z0-9 chars and -,_ ; therefore, 64 possibilities.
+  end
+
+  def remember # this method will assign a random string to serve as an identifier for cookies; unlike 'session', the 'cookie' method is not secure by default.
+    self.remember_token = User.new_token # won't actually save this value in a db.
+    update_attribute(:remember_digest, User.digest(remember_token) ) # the method 'update_attribute' bypasses db validations; the generated string is encrypted before being saved on the db so a potential attacker cannot hijack a session using XSS attacks or by capturing the token with a packet sniffer.
+  end
+
+  def authenticate # returns true if the given token (which will be automatically sent by the user's browser) matches the digest.
+    BCrypt::Password.new(:remember_digest).is_password?(remember_token)
   end
 
 end
