c9b4b2031e801822108b60e9580c498885c244a5
otamm <oivatom@gmail.com>
2015-01-27 02:30:24 -0200

    Further validations and tests for post

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,5 @@
 class Micropost < ActiveRecord::Base
   belongs_to :user
   validates :user_id, presence: true # post is only valid if it is associated with an user.
+  validates :content, presence: true, length: { maximum: 140 } # post must have a content, content must have 140 chars at max.
 end
