4186013503435c2595e0f4e0d007098cbcdcb6eb
otamm <oivatom@gmail.com>
2015-01-28 04:34:58 -0200

    Correctly ordered microposts & wrote tests for it

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -4,7 +4,7 @@ class MicropostTest < ActiveSupport::TestCase
 
   def setup
     @user = users(:bob)
-    @micropost = @user.microposts.build(content: "Lorem ipsum", user_id: @user.id) # a micropost is necessarily associated with an user. Note that the method is '.build()' is different from '.create()' because '.build()' returns an object while '.create()' returns and saves to the DB.
+    @micropost = @user.microposts.build(content: "Lorem ipsum") # ', user_id: @user.id)': no longer necessary, the micropost is already associated with the user. # a micropost is necessarily associated with an user. Note that the method is '.build()' is different from '.create()' because '.build()' returns an object while '.create()' returns and saves to the DB.
   end
 
   test "should be valid" do
@@ -24,6 +24,10 @@ class MicropostTest < ActiveSupport::TestCase
   test "content should be at most 140 characters" do
     @micropost.content = "a" * 141
     assert_not @micropost.valid?
+  end
+
+  test "order should be from most to least recent" do
+    assert_equal Micropost.first, microposts(:most_recent)
   end
 
 end
