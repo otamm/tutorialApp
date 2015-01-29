88d9cbfd724257107860bc7b4f954bc033e9e721
otamm <oivatom@gmail.com>
2015-01-29 17:43:33 -0200

    Coded form for micropost posting

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,11 +2,23 @@ class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy] # execute 'logged_in_user' before the actions specified on 'only: []'.
 
   def create
-
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
   end
 
   def destroy
 
+  end
+
+  private
+
+  def micropost_params
+    params.require(:micropost).permit(:content) # only allows posting of a micropost's :content
   end
 
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
+    @micropost = current_user.microposts.build if logged_in? # 'current_user' only exists if the user is logged, so the variable is only defined that way.
   end
 
   def help
