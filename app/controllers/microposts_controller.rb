e86c375fd518e1f24e042e7a676129f0c058bada
otamm <oivatom@gmail.com>
2015-01-30 06:43:06 -0200

    Implemented micropost deletion

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy] # execute 'logged_in_user' before the actions specified on 'only: []'.
+  before_action :correct_user, only: [:destroy]
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -13,13 +14,20 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
-
+    @micropost.destroy
+    flash[:success] = "Micropost deleted."
+    redirect_to request.referrer || root_url # request.referrer is more or less like request.url used in friendly forwarding, but it automatically redirects back to the last URL visited.
   end
 
   private
 
   def micropost_params
     params.require(:micropost).permit(:content) # only allows posting of a micropost's :content
+  end
+
+  def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id]) # checks if the sent URL contains the id of a micropost by the current user.
+    redirect_to root_url if @micropost.nil?
   end
 
 end
